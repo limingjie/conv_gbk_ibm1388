b3a289519304bc1c4fa4a4805874892c02ce598a
Mingjie Li <limingjie@outlook.com>
2014-11-07 14:30:31 +0800

    Convert tab to spaces

--- gbk_to_ibm1388.c
+++ gbk_to_ibm1388.c
@@ -5,14 +5,14 @@
 /* map of GBK to IBM-1388, basic ASCII to EBCDIC part */
 /* only 126 ASCII chars valid */
 char A2E[129] =
-	"\x00\x01\x02\x03\x37\x2D\x2E\x2F\x16\x05\x25\x0B\x0C\x0D\x00\x00"
-	"\x10\x11\x12\x13\x3C\x3D\x32\x26\x18\x19\x3F\x27\x1C\x1D\x1E\x1F"
-	"\x40\x5A\x7F\x7B\xE0\x6C\x50\x7D\x4D\x5D\x5C\x4E\x6B\x60\x4B\x61"
-	"\xF0\xF1\xF2\xF3\xF4\xF5\xF6\xF7\xF8\xF9\x7A\x5E\x4C\x7E\x6E\x6F"
-	"\x7C\xC1\xC2\xC3\xC4\xC5\xC6\xC7\xC8\xC9\xD1\xD2\xD3\xD4\xD5\xD6"
-	"\xD7\xD8\xD9\xE2\xE3\xE4\xE5\xE6\xE7\xE8\xE9\xBA\xB2\xBB\xB0\x6D"
-	"\x79\x81\x82\x83\x84\x85\x86\x87\x88\x89\x91\x92\x93\x94\x95\x96"
-	"\x97\x98\x99\xA2\xA3\xA4\xA5\xA6\xA7\xA8\xA9\xC0\x4F\xD0\xA0\x07";
+    "\x00\x01\x02\x03\x37\x2D\x2E\x2F\x16\x05\x25\x0B\x0C\x0D\x00\x00"
+    "\x10\x11\x12\x13\x3C\x3D\x32\x26\x18\x19\x3F\x27\x1C\x1D\x1E\x1F"
+    "\x40\x5A\x7F\x7B\xE0\x6C\x50\x7D\x4D\x5D\x5C\x4E\x6B\x60\x4B\x61"
+    "\xF0\xF1\xF2\xF3\xF4\xF5\xF6\xF7\xF8\xF9\x7A\x5E\x4C\x7E\x6E\x6F"
+    "\x7C\xC1\xC2\xC3\xC4\xC5\xC6\xC7\xC8\xC9\xD1\xD2\xD3\xD4\xD5\xD6"
+    "\xD7\xD8\xD9\xE2\xE3\xE4\xE5\xE6\xE7\xE8\xE9\xBA\xB2\xBB\xB0\x6D"
+    "\x79\x81\x82\x83\x84\x85\x86\x87\x88\x89\x91\x92\x93\x94\x95\x96"
+    "\x97\x98\x99\xA2\xA3\xA4\xA5\xA6\xA7\xA8\xA9\xC0\x4F\xD0\xA0\x07";
 
 /* map char array of GBK to IBM-1388 */
 unsigned char *c_mgi;
@@ -32,199 +32,199 @@ unsigned char *c_mgi;
 +-----------------------------------------------------------------------------*/
 int gbk_to_ibm1388(unsigned char *gbk, unsigned char *ibm)
 {
-	size_t offset;
-	size_t cnt = 0;
-	int is_last_single = 1; /* Yes(1), No(0) */
-	unsigned char *c = gbk;
+    size_t offset;
+    size_t cnt = 0;
+    int is_last_single = 1; /* Yes(1), No(0) */
+    unsigned char *c = gbk;
 
-	while (*c)
-	{
-		if (!(*c & 0x80))
-		{
-			/* End of double byte */
-			if (!is_last_single)
-			{
-				is_last_single = 1; /* Yes */
-				ibm[cnt++] = 0x0F;
-			}
+    while (*c)
+    {
+        if (!(*c & 0x80))
+        {
+            /* End of double byte */
+            if (!is_last_single)
+            {
+                is_last_single = 1; /* Yes */
+                ibm[cnt++] = 0x0F;
+            }
 
-			if (*c == 0x0E || *c == 0x0F)
-			{
-				/* Invalid GBK string */
-				return -1;
-			}
+            if (*c == 0x0E || *c == 0x0F)
+            {
+                /* Invalid GBK string */
+                return -1;
+            }
 
-			ibm[cnt++] = A2E[*c++];
-		}
-		else
-		{
-			/* check high byte */
-			if (*c < 0x81 || *c > 0xFE)
-			{
-				/* Invalid GBK string */
-				return -1;
-			}
+            ibm[cnt++] = A2E[*c++];
+        }
+        else
+        {
+            /* check high byte */
+            if (*c < 0x81 || *c > 0xFE)
+            {
+                /* Invalid GBK string */
+                return -1;
+            }
 
-			/* calculate base offset by high byte */
-			offset = (*c++ - 0x81) * 0xBF;
+            /* calculate base offset by high byte */
+            offset = (*c++ - 0x81) * 0xBF;
 
-			/* check low byte */
-			if (*c < 0x40 || *c == 0x7F || *c > 0xFE)
-			{
-				/* Invalid GBK string */
-				return -1;
-			}
+            /* check low byte */
+            if (*c < 0x40 || *c == 0x7F || *c > 0xFE)
+            {
+                /* Invalid GBK string */
+                return -1;
+            }
 
-			/* calculate exact offset by low byte */
-			offset += *c++ - 0x40;
-			offset *= 2;
+            /* calculate exact offset by low byte */
+            offset += *c++ - 0x40;
+            offset *= 2;
 
-			/* Start of double byte */
-			if (is_last_single)
-			{
-				is_last_single = 0;  /* No */
-				ibm[cnt++] = 0x0E;
-			}
+            /* Start of double byte */
+            if (is_last_single)
+            {
+                is_last_single = 0;  /* No */
+                ibm[cnt++] = 0x0E;
+            }
 
-			ibm[cnt++] = c_mgi[offset++];
-			ibm[cnt++] = c_mgi[offset];
-		}
-	}
+            ibm[cnt++] = c_mgi[offset++];
+            ibm[cnt++] = c_mgi[offset];
+        }
+    }
 
-	/* End of double byte */
-	if (!is_last_single) ibm[cnt++] = 0x0F;
+    /* End of double byte */
+    if (!is_last_single) ibm[cnt++] = 0x0F;
 
-	/* Finish string */
-	ibm[cnt] = '\0';
+    /* Finish string */
+    ibm[cnt] = '\0';
 
-	return cnt;
+    return cnt;
 }
 
 int test_valid()
 {
-	int length;
-	unsigned char result[100];
+    int length;
+    unsigned char result[100];
 
-	length = gbk_to_ibm1388((unsigned char *)"1234567890", result);
-	if (length > 0) printf("%s\x0D", result);
+    length = gbk_to_ibm1388((unsigned char *)"1234567890", result);
+    if (length > 0) printf("%s\x0D", result);
 
-	length = gbk_to_ibm1388((unsigned char *)"~!@#$%^&*()_+`-=[];',./{}:<>?|", result);
-	if (length > 0) printf("%s\x0D", result);
+    length = gbk_to_ibm1388((unsigned char *)"~!@#$%^&*()_+`-=[];',./{}:<>?|", result);
+    if (length > 0) printf("%s\x0D", result);
 
-	length = gbk_to_ibm1388((unsigned char *)"abcdefghijklmnopqrstuvwxyz", result);
-	if (length > 0) printf("%s\x0D", result);
+    length = gbk_to_ibm1388((unsigned char *)"abcdefghijklmnopqrstuvwxyz", result);
+    if (length > 0) printf("%s\x0D", result);
 
-	length = gbk_to_ibm1388((unsigned char *)"ABCDEFGHIJKLMNOPQRSTUVWXYZ", result);
-	if (length > 0) printf("%s\x0D", result);
+    length = gbk_to_ibm1388((unsigned char *)"ABCDEFGHIJKLMNOPQRSTUVWXYZ", result);
+    if (length > 0) printf("%s\x0D", result);
 
-	length = gbk_to_ibm1388((unsigned char *)"≤‚ ‘≤‚ ‘≤‚ ‘", result);
-	if (length > 0) printf("%s\x0D", result);
+    length = gbk_to_ibm1388((unsigned char *)"≤‚ ‘≤‚ ‘≤‚ ‘", result);
+    if (length > 0) printf("%s\x0D", result);
 
-	length = gbk_to_ibm1388((unsigned char *)"*≤‚* ‘*≤‚* ‘*≤‚* ‘*", result);
-	if (length > 0) printf("%s\x0D", result);
+    length = gbk_to_ibm1388((unsigned char *)"*≤‚* ‘*≤‚* ‘*≤‚* ‘*", result);
+    if (length > 0) printf("%s\x0D", result);
 
-	length = gbk_to_ibm1388((unsigned char *)"≤‚ ‘***≤‚ ‘***≤‚ ‘", result);
-	if (length > 0) printf("%s\x0D", result);
+    length = gbk_to_ibm1388((unsigned char *)"≤‚ ‘***≤‚ ‘***≤‚ ‘", result);
+    if (length > 0) printf("%s\x0D", result);
 
-	length = gbk_to_ibm1388((unsigned char *)"***≤‚ ‘≤‚ ‘≤‚ ‘***", result);
-	if (length > 0) printf("%s\x0D", result);
+    length = gbk_to_ibm1388((unsigned char *)"***≤‚ ‘≤‚ ‘≤‚ ‘***", result);
+    if (length > 0) printf("%s\x0D", result);
 
-	length = gbk_to_ibm1388((unsigned char *)"***≤‚ ‘≤‚ ‘≤‚ ‘", result);
-	if (length > 0) printf("%s\x0D", result);
+    length = gbk_to_ibm1388((unsigned char *)"***≤‚ ‘≤‚ ‘≤‚ ‘", result);
+    if (length > 0) printf("%s\x0D", result);
 
-	length = gbk_to_ibm1388((unsigned char *)"≤‚ ‘≤‚ ‘≤‚ ‘***", result);
-	if (length > 0) printf("%s\x0D", result);
+    length = gbk_to_ibm1388((unsigned char *)"≤‚ ‘≤‚ ‘≤‚ ‘***", result);
+    if (length > 0) printf("%s\x0D", result);
 
-	length = gbk_to_ibm1388((unsigned char *)"***≤‚ ‘***≤‚ ‘***≤‚ ‘***", result);
-	if (length > 0) printf("%s\x0D", result);
+    length = gbk_to_ibm1388((unsigned char *)"***≤‚ ‘***≤‚ ‘***≤‚ ‘***", result);
+    if (length > 0) printf("%s\x0D", result);
 
-	length = gbk_to_ibm1388((unsigned char *)"***≤‚*** ‘≤‚ ‘≤‚*** ‘***", result);
-	if (length > 0) printf("%s\x0D", result);
+    length = gbk_to_ibm1388((unsigned char *)"***≤‚*** ‘≤‚ ‘≤‚*** ‘***", result);
+    if (length > 0) printf("%s\x0D", result);
 
-	return 0;
+    return 0;
 }
 
 int test_invalid()
 {
-	int length;
-	unsigned char result[100];
+    int length;
+    unsigned char result[100];
 
-	length = gbk_to_ibm1388((unsigned char *)"≤", result);
-	if (length < 0) printf("1 Invalid!\n");
+    length = gbk_to_ibm1388((unsigned char *)"≤", result);
+    if (length < 0) printf("1 Invalid!\n");
 
-	length = gbk_to_ibm1388((unsigned char *)"≤***", result);
-	if (length < 0) printf("2 Invalid!\n");
+    length = gbk_to_ibm1388((unsigned char *)"≤***", result);
+    if (length < 0) printf("2 Invalid!\n");
 
-	length = gbk_to_ibm1388((unsigned char *)"***≤", result);
-	if (length < 0) printf("3 Invalid!\n");
+    length = gbk_to_ibm1388((unsigned char *)"***≤", result);
+    if (length < 0) printf("3 Invalid!\n");
 
-	length = gbk_to_ibm1388((unsigned char *)"≤‚ ‘≤‚ ‘≤‚ ", result);
-	if (length < 0) printf("4 Invalid!\n");
+    length = gbk_to_ibm1388((unsigned char *)"≤‚ ‘≤‚ ‘≤‚ ", result);
+    if (length < 0) printf("4 Invalid!\n");
 
-	length = gbk_to_ibm1388((unsigned char *)"≤‚ ‘≤‚ ‘ ‚ ‘", result);
-	if (length < 0) printf("5 Invalid!\n");
+    length = gbk_to_ibm1388((unsigned char *)"≤‚ ‘≤‚ ‘ ‚ ‘", result);
+    if (length < 0) printf("5 Invalid!\n");
 
-	length = gbk_to_ibm1388((unsigned char *)"≤‚ ‘≤‚ ‘‚ ‘", result);
-	if (length < 0) printf("6 Invalid!\n");
+    length = gbk_to_ibm1388((unsigned char *)"≤‚ ‘≤‚ ‘‚ ‘", result);
+    if (length < 0) printf("6 Invalid!\n");
 
-	length = gbk_to_ibm1388((unsigned char *)"Ä‚ ‘≤‚ ‘≤‚ ‘", result);
-	if (length < 0) printf("7 Invalid!\n");
+    length = gbk_to_ibm1388((unsigned char *)"Ä‚ ‘≤‚ ‘≤‚ ‘", result);
+    if (length < 0) printf("7 Invalid!\n");
 
-	length = gbk_to_ibm1388((unsigned char *)"≤9 ‘≤‚ ‘≤‚ ‘", result);
-	if (length < 0) printf("8 Invalid!\n");
+    length = gbk_to_ibm1388((unsigned char *)"≤9 ‘≤‚ ‘≤‚ ‘", result);
+    if (length < 0) printf("8 Invalid!\n");
 
-	length = gbk_to_ibm1388((unsigned char *)"≤ ‘≤‚ ‘≤‚ ‘", result);
-	if (length < 0) printf("9 Invalid!\n");
+    length = gbk_to_ibm1388((unsigned char *)"≤ ‘≤‚ ‘≤‚ ‘", result);
+    if (length < 0) printf("9 Invalid!\n");
 
-	return 0;
+    return 0;
 }
 
 int test_perf()
 {
-	int i;
-	unsigned char result[200];
+    int i;
+    unsigned char result[200];
 
-	for (i = 0; i < 10000000; i++)
-	{
-		gbk_to_ibm1388
-		(
-			(unsigned char *)
-				"~!@#$%^&*()_+`-=[];',./{}:<>?|1234567890-=][';/."
-				"≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘"
-				"≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘",
-			result
-		);
-	}
+    for (i = 0; i < 10000000; i++)
+    {
+        gbk_to_ibm1388
+        (
+            (unsigned char *)
+                "~!@#$%^&*()_+`-=[];',./{}:<>?|1234567890-=][';/."
+                "≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘"
+                "≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘≤‚ ‘",
+            result
+        );
+    }
 
-	return 0;
+    return 0;
 }
 
 int main()
 {
-	/* map file handle of GBK to IBM-1388 */
-	FILE *f_mgi;
+    /* map file handle of GBK to IBM-1388 */
+    FILE *f_mgi;
 
-	/* Open GBK to IBM-1388 map */
-	f_mgi = fopen("gbkto1388.map", "rb");
-	if (f_mgi == NULL) exit(1);
+    /* Open GBK to IBM-1388 map */
+    f_mgi = fopen("gbkto1388.map", "rb");
+    if (f_mgi == NULL) exit(1);
 
-	/* Allocate buffer for map */
-	c_mgi = (unsigned char *)malloc(sizeof(unsigned char) * 48132);
-	if (c_mgi == NULL) exit(1);
+    /* Allocate buffer for map */
+    c_mgi = (unsigned char *)malloc(sizeof(unsigned char) * 48132);
+    if (c_mgi == NULL) exit(1);
 
-	/* Read map to memory */
-	size_t cnt = fread(c_mgi, sizeof(unsigned char), 48132, f_mgi);
-	if (cnt != 48132) exit(1);
+    /* Read map to memory */
+    size_t cnt = fread(c_mgi, sizeof(unsigned char), 48132, f_mgi);
+    if (cnt != 48132) exit(1);
 
-	/* Close GBK to IBM-1388 map */
-	fclose(f_mgi);
+    /* Close GBK to IBM-1388 map */
+    fclose(f_mgi);
 
-	test_valid();
-	/* test_invalid(); */
-	/* test_perf(); */
+    test_valid();
+    /* test_invalid(); */
+    /* test_perf(); */
 
-	/* Free map memory */
-	free(c_mgi);
+    /* Free map memory */
+    free(c_mgi);
 
-	return 0;
+    return 0;
 }
--- ibm1388_to_gbk.c
+++ ibm1388_to_gbk.c
@@ -5,22 +5,22 @@
 /* map of IBM-1388 to GBK, basic EBCDIC to ASCII part */
 /* only 126 EBCDIC chars valid */
 char E2A[257] =
-	"\x00\x01\x02\x03\x00\x09\x00\x7F\x00\x00\x00\x0B\x0C\x0D\x00\x00"
-	"\x10\x11\x12\x13\x00\x00\x08\x00\x18\x19\x00\x00\x1C\x1D\x1E\x1F"
-	"\x00\x00\x00\x00\x00\x0A\x17\x1B\x00\x00\x00\x00\x00\x05\x06\x07"
-	"\x00\x00\x16\x00\x00\x00\x00\x04\x00\x00\x00\x00\x14\x15\x00\x1A"
-	"\x20\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x2E\x3C\x28\x2B\x7C"
-	"\x26\x00\x00\x00\x00\x00\x00\x00\x00\x00\x21\x00\x2A\x29\x3B\x00"
-	"\x2D\x2F\x00\x00\x00\x00\x00\x00\x00\x00\x00\x2C\x25\x5F\x3E\x3F"
-	"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x60\x3A\x23\x40\x27\x3D\x22"
-	"\x00\x61\x62\x63\x64\x65\x66\x67\x68\x69\x00\x00\x00\x00\x00\x00"
-	"\x00\x6A\x6B\x6C\x6D\x6E\x6F\x70\x71\x72\x00\x00\x00\x00\x00\x00"
-	"\x7E\x00\x73\x74\x75\x76\x77\x78\x79\x7A\x00\x00\x00\x00\x00\x00"
-	"\x5E\x00\x5C\x00\x00\x00\x00\x00\x00\x00\x5B\x5D\x00\x00\x00\x00"
-	"\x7B\x41\x42\x43\x44\x45\x46\x47\x48\x49\x00\x00\x00\x00\x00\x00"
-	"\x7D\x4A\x4B\x4C\x4D\x4E\x4F\x50\x51\x52\x00\x00\x00\x00\x00\x00"
-	"\x24\x00\x53\x54\x55\x56\x57\x58\x59\x5A\x00\x00\x00\x00\x00\x00"
-	"\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x00\x00\x00\x00\x00\x00";
+    "\x00\x01\x02\x03\x00\x09\x00\x7F\x00\x00\x00\x0B\x0C\x0D\x00\x00"
+    "\x10\x11\x12\x13\x00\x00\x08\x00\x18\x19\x00\x00\x1C\x1D\x1E\x1F"
+    "\x00\x00\x00\x00\x00\x0A\x17\x1B\x00\x00\x00\x00\x00\x05\x06\x07"
+    "\x00\x00\x16\x00\x00\x00\x00\x04\x00\x00\x00\x00\x14\x15\x00\x1A"
+    "\x20\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x2E\x3C\x28\x2B\x7C"
+    "\x26\x00\x00\x00\x00\x00\x00\x00\x00\x00\x21\x00\x2A\x29\x3B\x00"
+    "\x2D\x2F\x00\x00\x00\x00\x00\x00\x00\x00\x00\x2C\x25\x5F\x3E\x3F"
+    "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x60\x3A\x23\x40\x27\x3D\x22"
+    "\x00\x61\x62\x63\x64\x65\x66\x67\x68\x69\x00\x00\x00\x00\x00\x00"
+    "\x00\x6A\x6B\x6C\x6D\x6E\x6F\x70\x71\x72\x00\x00\x00\x00\x00\x00"
+    "\x7E\x00\x73\x74\x75\x76\x77\x78\x79\x7A\x00\x00\x00\x00\x00\x00"
+    "\x5E\x00\x5C\x00\x00\x00\x00\x00\x00\x00\x5B\x5D\x00\x00\x00\x00"
+    "\x7B\x41\x42\x43\x44\x45\x46\x47\x48\x49\x00\x00\x00\x00\x00\x00"
+    "\x7D\x4A\x4B\x4C\x4D\x4E\x4F\x50\x51\x52\x00\x00\x00\x00\x00\x00"
+    "\x24\x00\x53\x54\x55\x56\x57\x58\x59\x5A\x00\x00\x00\x00\x00\x00"
+    "\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x00\x00\x00\x00\x00\x00";
 
 /* map char array of IBM-1388 to GBK */
 unsigned char *c_mig;
@@ -40,173 +40,173 @@ unsigned char *c_mig;
 +-----------------------------------------------------------------------------*/
 int ibm1388_to_gbk(unsigned char *ibm, unsigned char *gbk)
 {
-	size_t offset;
-	size_t g_cnt = 0;
-	int is_single = 1; /* Yes(1), No(0) */
-	unsigned char *c = ibm;
+    size_t offset;
+    size_t g_cnt = 0;
+    int is_single = 1; /* Yes(1), No(0) */
+    unsigned char *c = ibm;
 
-	while (*c)
-	{
-		if (is_single)
-		{
-			/* start of double byte */
-			if (*c == 0x0E)
-			{
-				is_single = 0;
-				c++;
-				continue;
-			}
+    while (*c)
+    {
+        if (is_single)
+        {
+            /* start of double byte */
+            if (*c == 0x0E)
+            {
+                is_single = 0;
+                c++;
+                continue;
+            }
 
-			/* check current char */
-			if (E2A[*c] == 0x00 && *c != 0x00)
-			{
-				/* Invalid IBM-1388 string */
-				return -1;
-			}
+            /* check current char */
+            if (E2A[*c] == 0x00 && *c != 0x00)
+            {
+                /* Invalid IBM-1388 string */
+                return -1;
+            }
 
-			gbk[g_cnt++] = E2A[*c++];
-		}
-		else
-		{
-			/* end of double byte */
-			if (*c == 0x0F)
-			{
-				is_single = 1;
-				c++;
-				continue;
-			}
+            gbk[g_cnt++] = E2A[*c++];
+        }
+        else
+        {
+            /* end of double byte */
+            if (*c == 0x0F)
+            {
+                is_single = 1;
+                c++;
+                continue;
+            }
 
-			/* check high byte */
-			if (*c < 0x40 || *c > 0xFF)
-			{
-				/* Invalid IBM-1388 string */
-				return -1;
-			}
+            /* check high byte */
+            if (*c < 0x40 || *c > 0xFF)
+            {
+                /* Invalid IBM-1388 string */
+                return -1;
+            }
 
-			/* calculate base offset by high byte */
-			offset = (*c++ - 0x40) * 0xc0;
+            /* calculate base offset by high byte */
+            offset = (*c++ - 0x40) * 0xc0;
 
-			/* check low byte */
-			if (*c < 0x40 || *c > 0xFF)
-			{
-				/* Invalid IBM-1388 string */
-				return -1;
-			}
+            /* check low byte */
+            if (*c < 0x40 || *c > 0xFF)
+            {
+                /* Invalid IBM-1388 string */
+                return -1;
+            }
 
-			/* calculate exact offset by low byte */
-			offset += *c++ - 0x40;
-			offset *= 2;
+            /* calculate exact offset by low byte */
+            offset += *c++ - 0x40;
+            offset *= 2;
 
-			gbk[g_cnt++] = c_mig[offset++];
-			gbk[g_cnt++] = c_mig[offset];
-		}
-	}
+            gbk[g_cnt++] = c_mig[offset++];
+            gbk[g_cnt++] = c_mig[offset];
+        }
+    }
 
-	/* Error end of string, missed 0x0F */
-	if (!is_single) return -1;
+    /* Error end of string, missed 0x0F */
+    if (!is_single) return -1;
 
-	gbk[g_cnt] = '\0';
+    gbk[g_cnt] = '\0';
 
-	return g_cnt;
+    return g_cnt;
 }
 
 int test_valid()
 {
-	int rc;
-	unsigned char gbk[200];
+    int rc;
+    unsigned char gbk[200];
 
-	rc = ibm1388_to_gbk((unsigned char *)"\x0E\x0F", gbk);
-	printf("%d - %s\n", rc, gbk);
-	rc = ibm1388_to_gbk((unsigned char *)"\xF0\xF1\xF2\xF3\xF4\xF5\xF6\xF7\xF8\xF9", gbk);
-	printf("%d - %s\n", rc, gbk);
-	rc = ibm1388_to_gbk((unsigned char *)"\xa0\x5a\x7c\x7b\xe0\x6c\xb0\x50\x5c\x4d\x5d\x6d\x4e\x79\x60\x7e\xba\xbb\x5e\x7d\x6b\x4b\x61\xc0\xd0\x7a\x4c\x6e\x6f\x4f", gbk);
-	printf("%d - %s\n", rc, gbk);
-	rc = ibm1388_to_gbk((unsigned char *)"\x81\x82\x83\x84\x85\x86\x87\x88\x89\x91\x92\x93\x94\x95\x96\x97\x98\x99\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9", gbk);
-	printf("%d - %s\n", rc, gbk);
-	rc = ibm1388_to_gbk((unsigned char *)"\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9", gbk);
-	printf("%d - %s\n", rc, gbk);
-	rc = ibm1388_to_gbk((unsigned char *)"\x0e\x49\xe1\x55\xd3\x49\xe1\x55\xd3\x49\xe1\x55\xd3\x0f", gbk);
-	printf("%d - %s\n", rc, gbk);
-	rc = ibm1388_to_gbk((unsigned char *)"\x5c\x0e\x49\xe1\x0f\x5c\x0e\x55\xd3\x0f\x5c\x0e\x49\xe1\x0f\x5c\x0e\x55\xd3\x0f\x5c\x0e\x49\xe1\x0f\x5c\x0e\x55\xd3\x0f\x5c", gbk);
-	printf("%d - %s\n", rc, gbk);
-	rc = ibm1388_to_gbk((unsigned char *)"\x0e\x49\xe1\x55\xd3\x0f\x5c\x5c\x5c\x0e\x49\xe1\x55\xd3\x0f\x5c\x5c\x5c\x0e\x49\xe1\x55\xd3\x0f", gbk);
-	printf("%d - %s\n", rc, gbk);
-	rc = ibm1388_to_gbk((unsigned char *)"\x5c\x5c\x5c\x0e\x49\xe1\x55\xd3\x49\xe1\x55\xd3\x49\xe1\x55\xd3\x0f\x5c\x5c\x5c", gbk);
-	printf("%d - %s\n", rc, gbk);
-	rc = ibm1388_to_gbk((unsigned char *)"\x5c\x5c\x5c\x0e\x49\xe1\x55\xd3\x49\xe1\x55\xd3\x49\xe1\x55\xd3\x0f", gbk);
-	printf("%d - %s\n", rc, gbk);
-	rc = ibm1388_to_gbk((unsigned char *)"\x0e\x49\xe1\x55\xd3\x49\xe1\x55\xd3\x49\xe1\x55\xd3\x0f\x5c\x5c\x5c", gbk);
-	printf("%d - %s\n", rc, gbk);
-	rc = ibm1388_to_gbk((unsigned char *)"\x5c\x5c\x5c\x0e\x49\xe1\x55\xd3\x0f\x5c\x5c\x5c\x0e\x49\xe1\x55\xd3\x0f\x5c\x5c\x5c\x0e\x49\xe1\x55\xd3\x0f\x5c\x5c\x5c", gbk);
-	printf("%d - %s\n", rc, gbk);
-	rc = ibm1388_to_gbk((unsigned char *)"\x5c\x5c\x5c\x0e\x49\xe1\x0f\x5c\x5c\x5c\x0e\x55\xd3\x49\xe1\x55\xd3\x49\xe1\x0f\x5c\x5c\x5c\x0e\x55\xd3\x0f\x5c\x5c\x5c", gbk);
-	printf("%d - %s\n", rc, gbk);
+    rc = ibm1388_to_gbk((unsigned char *)"\x0E\x0F", gbk);
+    printf("%d - %s\n", rc, gbk);
+    rc = ibm1388_to_gbk((unsigned char *)"\xF0\xF1\xF2\xF3\xF4\xF5\xF6\xF7\xF8\xF9", gbk);
+    printf("%d - %s\n", rc, gbk);
+    rc = ibm1388_to_gbk((unsigned char *)"\xa0\x5a\x7c\x7b\xe0\x6c\xb0\x50\x5c\x4d\x5d\x6d\x4e\x79\x60\x7e\xba\xbb\x5e\x7d\x6b\x4b\x61\xc0\xd0\x7a\x4c\x6e\x6f\x4f", gbk);
+    printf("%d - %s\n", rc, gbk);
+    rc = ibm1388_to_gbk((unsigned char *)"\x81\x82\x83\x84\x85\x86\x87\x88\x89\x91\x92\x93\x94\x95\x96\x97\x98\x99\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9", gbk);
+    printf("%d - %s\n", rc, gbk);
+    rc = ibm1388_to_gbk((unsigned char *)"\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9", gbk);
+    printf("%d - %s\n", rc, gbk);
+    rc = ibm1388_to_gbk((unsigned char *)"\x0e\x49\xe1\x55\xd3\x49\xe1\x55\xd3\x49\xe1\x55\xd3\x0f", gbk);
+    printf("%d - %s\n", rc, gbk);
+    rc = ibm1388_to_gbk((unsigned char *)"\x5c\x0e\x49\xe1\x0f\x5c\x0e\x55\xd3\x0f\x5c\x0e\x49\xe1\x0f\x5c\x0e\x55\xd3\x0f\x5c\x0e\x49\xe1\x0f\x5c\x0e\x55\xd3\x0f\x5c", gbk);
+    printf("%d - %s\n", rc, gbk);
+    rc = ibm1388_to_gbk((unsigned char *)"\x0e\x49\xe1\x55\xd3\x0f\x5c\x5c\x5c\x0e\x49\xe1\x55\xd3\x0f\x5c\x5c\x5c\x0e\x49\xe1\x55\xd3\x0f", gbk);
+    printf("%d - %s\n", rc, gbk);
+    rc = ibm1388_to_gbk((unsigned char *)"\x5c\x5c\x5c\x0e\x49\xe1\x55\xd3\x49\xe1\x55\xd3\x49\xe1\x55\xd3\x0f\x5c\x5c\x5c", gbk);
+    printf("%d - %s\n", rc, gbk);
+    rc = ibm1388_to_gbk((unsigned char *)"\x5c\x5c\x5c\x0e\x49\xe1\x55\xd3\x49\xe1\x55\xd3\x49\xe1\x55\xd3\x0f", gbk);
+    printf("%d - %s\n", rc, gbk);
+    rc = ibm1388_to_gbk((unsigned char *)"\x0e\x49\xe1\x55\xd3\x49\xe1\x55\xd3\x49\xe1\x55\xd3\x0f\x5c\x5c\x5c", gbk);
+    printf("%d - %s\n", rc, gbk);
+    rc = ibm1388_to_gbk((unsigned char *)"\x5c\x5c\x5c\x0e\x49\xe1\x55\xd3\x0f\x5c\x5c\x5c\x0e\x49\xe1\x55\xd3\x0f\x5c\x5c\x5c\x0e\x49\xe1\x55\xd3\x0f\x5c\x5c\x5c", gbk);
+    printf("%d - %s\n", rc, gbk);
+    rc = ibm1388_to_gbk((unsigned char *)"\x5c\x5c\x5c\x0e\x49\xe1\x0f\x5c\x5c\x5c\x0e\x55\xd3\x49\xe1\x55\xd3\x49\xe1\x0f\x5c\x5c\x5c\x0e\x55\xd3\x0f\x5c\x5c\x5c", gbk);
+    printf("%d - %s\n", rc, gbk);
 
-	return 0;
+    return 0;
 }
 
 int test_invalid()
 {
-	int rc;
-	unsigned char gbk[200];
+    int rc;
+    unsigned char gbk[200];
 
-	rc = ibm1388_to_gbk((unsigned char *)"\x0E", gbk);
-	if (rc < 0) printf("1 Invalid!\n");
+    rc = ibm1388_to_gbk((unsigned char *)"\x0E", gbk);
+    if (rc < 0) printf("1 Invalid!\n");
 
-	rc = ibm1388_to_gbk((unsigned char *)"\x0E\xF0\xF1\xF2", gbk);
-	if (rc < 0) printf("2 Invalid!\n");
+    rc = ibm1388_to_gbk((unsigned char *)"\x0E\xF0\xF1\xF2", gbk);
+    if (rc < 0) printf("2 Invalid!\n");
 
-	rc = ibm1388_to_gbk((unsigned char *)"\x0E\x0E", gbk);
-	if (rc < 0) printf("3 Invalid!\n");
+    rc = ibm1388_to_gbk((unsigned char *)"\x0E\x0E", gbk);
+    if (rc < 0) printf("3 Invalid!\n");
 
-	rc = ibm1388_to_gbk((unsigned char *)"\x0F", gbk);
-	if (rc < 0) printf("4 Invalid!\n");
+    rc = ibm1388_to_gbk((unsigned char *)"\x0F", gbk);
+    if (rc < 0) printf("4 Invalid!\n");
 
-	rc = ibm1388_to_gbk((unsigned char *)"\xF0\xF1\xF2\x0F", gbk);
-	if (rc < 0) printf("5 Invalid!\n");
+    rc = ibm1388_to_gbk((unsigned char *)"\xF0\xF1\xF2\x0F", gbk);
+    if (rc < 0) printf("5 Invalid!\n");
 
-	rc = ibm1388_to_gbk((unsigned char *)"\x41", gbk);
-	if (rc < 0) printf("6 Invalid!\n");
+    rc = ibm1388_to_gbk((unsigned char *)"\x41", gbk);
+    if (rc < 0) printf("6 Invalid!\n");
 
-	rc = ibm1388_to_gbk((unsigned char *)"\x0e\x39\x81\x0f", gbk);
-	if (rc < 0) printf("7 Invalid!\n");
+    rc = ibm1388_to_gbk((unsigned char *)"\x0e\x39\x81\x0f", gbk);
+    if (rc < 0) printf("7 Invalid!\n");
 
-	rc = ibm1388_to_gbk((unsigned char *)"\x0e\x49\xe1\x55\x0f", gbk);
-	if (rc < 0) printf("8 Invalid!\n");
+    rc = ibm1388_to_gbk((unsigned char *)"\x0e\x49\xe1\x55\x0f", gbk);
+    if (rc < 0) printf("8 Invalid!\n");
 
-	rc = ibm1388_to_gbk((unsigned char *)"\x0e\x39\xe1\x55\xd3\x0f", gbk);
-	if (rc < 0) printf("9 Invalid!\n");
+    rc = ibm1388_to_gbk((unsigned char *)"\x0e\x39\xe1\x55\xd3\x0f", gbk);
+    if (rc < 0) printf("9 Invalid!\n");
 
-	rc = ibm1388_to_gbk((unsigned char *)"\x0e\x49\x21\x55\xd3\x0f", gbk);
-	if (rc < 0) printf("10 Invalid!\n");
+    rc = ibm1388_to_gbk((unsigned char *)"\x0e\x49\x21\x55\xd3\x0f", gbk);
+    if (rc < 0) printf("10 Invalid!\n");
 
-	return 0;
+    return 0;
 }
 
 int main()
 {
-	/* map file handle of IBM-1388 to GBK */
-	FILE *f_mig;
+    /* map file handle of IBM-1388 to GBK */
+    FILE *f_mig;
 
-	/* Open IBM-1388 to GBK map */
-	f_mig = fopen("1388togbk.map", "rb");
-	if (f_mig == NULL) exit(1);
+    /* Open IBM-1388 to GBK map */
+    f_mig = fopen("1388togbk.map", "rb");
+    if (f_mig == NULL) exit(1);
 
-	/* Allocate buffer for map */
-	c_mig = (unsigned char *)malloc(sizeof(unsigned char) * 73728);
-	if (c_mig == NULL) exit(1);
+    /* Allocate buffer for map */
+    c_mig = (unsigned char *)malloc(sizeof(unsigned char) * 73728);
+    if (c_mig == NULL) exit(1);
 
-	/* Read map to memory */
-	size_t cnt = fread(c_mig, sizeof(unsigned char), 73728, f_mig);
-	if (cnt != 73728) exit(1);
+    /* Read map to memory */
+    size_t cnt = fread(c_mig, sizeof(unsigned char), 73728, f_mig);
+    if (cnt != 73728) exit(1);
 
-	/* Close IBM-1388 to GBK map */
-	fclose(f_mig);
+    /* Close IBM-1388 to GBK map */
+    fclose(f_mig);
 
-	test_valid();
-	test_invalid();
+    test_valid();
+    test_invalid();
 
-	free(c_mig);
+    free(c_mig);
 
-	return 0;
+    return 0;
 }
